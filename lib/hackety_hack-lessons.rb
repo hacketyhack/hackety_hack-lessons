09012a95533a192d1886a12cfba338781bb0feff
Steve Klabnik <steve@steveklabnik.com>
2012-01-25 13:09:49 -0500

    properly namespacing

--- lib/hackety_hack-lessons.rb
+++
@@ -1,7 +0,0 @@
-require "hackety_hack-lessons/version"
-
-module HacketyHack
-  module Lessons
-    # Your code goes here...
-  end
-end
---
+++ lib/hackety_hack/lessons.rb
@@ -0,0 +1,8 @@
+require "hackety_hack/lessons/version"
+
+puts "Nothing to see here yet..."
+
+module HacketyHack
+  module Lessons
+  end
+end
